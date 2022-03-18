84c3632dc5004af9a221a0c52582f5babd8d12ed
Jok <jokhornsby@gmail.com>
2022-03-18 12:37:43 +0000

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('token')
+    const page: string = core.getInput('page')
+    const mdFile: string = core.getInput('mdFile')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    console.log(page)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
